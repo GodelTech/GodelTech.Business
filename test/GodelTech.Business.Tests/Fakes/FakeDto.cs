27a6a69987c8822eb5c619ff8ada251c70b292f6
Vladimir Rodchenko <vladimir@rodchenko.by>
2020-11-22 00:39:22 +0300

    add tests change version to 1.0.0

--- test/GodelTech.Business.Tests/Fakes/FakeDto.cs
+++ test/GodelTech.Business.Tests/Fakes/FakeDto.cs
@@ -1,8 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace GodelTech.Business.Tests.Fakes
+﻿namespace GodelTech.Business.Tests.Fakes
 {
-    [ExcludeFromCodeCoverage]
     public class FakeDto
     {
         // this is fake model and it is ok that it is empty
