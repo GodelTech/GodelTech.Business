d043456465c53a7fda7fc112e602df4844f42f31
Vladimir Rodchenko <vladimir@rodchenko.by>
2021-08-05 19:15:18 +0300

    solve CLSCompliant warning

--- src/GodelTech.Business/IBusinessService.cs
+++ src/GodelTech.Business/IBusinessService.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
+[assembly: CLSCompliant(true)]
 namespace GodelTech.Business
 {
     /// <summary>
