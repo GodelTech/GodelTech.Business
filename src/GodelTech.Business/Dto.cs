fce39babc86b3235904c46013c2f85229fd4cda0
Vladimir Rodchenko <vladimir@rodchenko.by>
2021-08-06 14:20:14 +0300

    add Dto and tests

---
+++ src/GodelTech.Business/Dto.cs
@@ -0,0 +1,15 @@
+﻿namespace GodelTech.Business
+{
+    /// <summary>
+    /// Data transfer object.
+    /// </summary>
+    /// <typeparam name="TType">The type of the T type.</typeparam>
+    public class Dto<TType> : IDto<TType>
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public virtual TType Id { get; set; }
+    }
+}
--- src/GodelTech.Business/IBusinessService.cs
+++ src/GodelTech.Business/IBusinessService.cs
@@ -12,8 +12,9 @@ namespace GodelTech.Business
     /// <typeparam name="TAddDto">The type of the T type.</typeparam>
     /// <typeparam name="TEditDto">The type of the T type.</typeparam>
     /// <typeparam name="TType">The type of the T type.</typeparam>
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("SonarCloud", "S2436:Reduce the number of generic parameters in the 'IBusinessService' interface to no more than the 2 authorized.", Justification = "<Pending>")]
+#pragma warning disable S2436 // Reduce the number of generic parameters in the 'IBusinessService' interface to no more than the 2 authorized.
     public interface IBusinessService<TDto, in TAddDto, in TEditDto, in TType>
+#pragma warning restore S2436 // Reduce the number of generic parameters in the 'IBusinessService' interface to no more than the 2 authorized.
         where TDto : class, IDto<TType>
         where TAddDto : class
         where TEditDto : class, IDto<TType>
